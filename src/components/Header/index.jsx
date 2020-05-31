235e7c00854e63db3974b5b11e688847e1f826c3
Kamal Hasan <>
2020-05-31 12:54:21 +0530

    Add sort toggler

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import "./style.css";
 
+const toggle = () => {
+  document.querySelectorAll(".sort").forEach(function (item) {
+    item.classList.toggle("active");
+  });
+};
+
 const Header = () => {
   return (
     <header className="app-header">
-      <img src="y18.gif" className="app-logo" alt="logo" />
+      <div>
+        <img src="y18.gif" className="app-logo" alt="logo" />
+        <span className="left-pad active sort" onClick={toggle}>
+          top
+        </span>
+        <span className="left-pad">|</span>
+        <span className="left-pad sort" onClick={toggle}>
+          new
+        </span>
+      </div>
     </header>
   );
 };
